b8a172dc0a744915f91c44b55c96d46f44c3f9b7
Ajay9970 <ajaytoradmal2009@gmail.com>
2020-08-02 23:57:13 +0530

    updated varibale

--- src/FintLargeNumberInArray.java
+++ src/FintLargeNumberInArray.java
@@ -3,7 +3,7 @@ public class FintLargeNumberInArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int array[]= {10,20,50,30,40};
+		int array[]= {10,20,50,10,40};
 		
 		int largeNumber=array[0];
 		for(int i=0;i<array.length;i++) {
